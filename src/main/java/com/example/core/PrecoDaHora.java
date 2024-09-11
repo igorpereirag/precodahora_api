0d6795b3f326dacd53cc0b22091fd5a9c5a6cca7
igorpereirag <igorpereira1501@gmail.com>
2024-09-11 11:56:26 -0300

    chore: refactor PrecoDaHora class to use HttpClient instead of Apache HttpClient

--- src/main/java/com/example/core/PrecoDaHora.java
+++ src/main/java/com/example/core/PrecoDaHora.java
@@ -1,85 +1,78 @@
 package com.example.core;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 public class PrecoDaHora {
-    private final CloseableHttpClient httpClient;
+    private final HttpClient httpClient;
     private final String baseUrl = "https://precodahora.ba.gov.br";
     private String csrfToken = "";
     private String cookies = "";
 
     public PrecoDaHora() {
-        this.httpClient = HttpClients.createDefault();
+        this.httpClient = HttpClient.newHttpClient();
     }
 
-    private String makePostRequest(String endpoint, Map<String, String> params) throws IOException {
-        HttpPost postRequest = new HttpPost(baseUrl + endpoint);
-
-        RequestConfig requestConfig = RequestConfig.custom()
-                .setConnectTimeout(5000)
-                .setSocketTimeout(5000)
-                .build();
-        postRequest.setConfig(requestConfig);
-
-        postRequest.setHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-        postRequest.setHeader("X-CSRFToken", csrfToken);
-        postRequest.setHeader("Cookie", cookies);
+    private String makePostRequest(String endpoint, Map<String, String> params) throws IOException, InterruptedException {
+        URI uri = URI.create(baseUrl + endpoint);
 
         StringBuilder body = new StringBuilder();
         for (Map.Entry<String, String> entry : params.entrySet()) {
             if (body.length() > 0) body.append("&");
             body.append(entry.getKey()).append("=").append(entry.getValue());
         }
-        postRequest.setEntity(new StringEntity(body.toString()));
 
-        try (CloseableHttpResponse response = httpClient.execute(postRequest)) {
-            HttpEntity entity = response.getEntity();
-            return EntityUtils.toString(entity);
-        }
+        HttpRequest postRequest = HttpRequest.newBuilder()
+                .uri(uri)
+                .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+                .header("X-CSRFToken", csrfToken)
+                .header("Cookie", cookies)
+                .POST(BodyPublishers.ofString(body.toString()))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
+        return response.body();
     }
 
-    public JSONObject post(String endpoint, Map<String, String> params) throws IOException {
+    public JSONObject post(String endpoint, Map<String, String> params) throws IOException, InterruptedException {
         String response = makePostRequest(endpoint, params);
         return new JSONObject(response);
     }
 
-    public Map<String, String> getCookiesAndCsrfToken() throws IOException {
-        HttpGet getRequest = new HttpGet(baseUrl + "/");
+    public Map<String, String> getCookiesAndCsrfToken() throws IOException, InterruptedException {
+        URI uri = URI.create(baseUrl + "/");
 
-        getRequest.setHeader("User-Agent", "Mozilla/5.0");
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(uri)
+                .header("User-Agent", "Mozilla/5.0")
+                .build();
 
-        try (CloseableHttpResponse response = httpClient.execute(getRequest)) {
-            String responseBody = EntityUtils.toString(response.getEntity());
+        HttpResponse<String> response = httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
+        String responseBody = response.body();
 
-            Document document = Jsoup.parse(responseBody);
-            Element csrfElement = document.selectFirst("#validate");
-            String csrfToken = csrfElement != null ? csrfElement.attr("data-id") : "";
-            String cookies = response.getFirstHeader("Set-Cookie") != null ? response.getFirstHeader("Set-Cookie").getValue() : "";
+        Document document = Jsoup.parse(responseBody);
+        Element csrfElement = document.selectFirst("#validate");
+        String csrfToken = csrfElement != null ? csrfElement.attr("data-id") : "";
+        String cookies = response.headers().firstValue("Set-Cookie").orElse("");
 
-            this.csrfToken = csrfToken;
-            this.cookies = cookies;
+        this.csrfToken = csrfToken;
+        this.cookies = cookies;
 
-            Map<String, String> result = new HashMap<>();
-            result.put("csrfToken", csrfToken);
-            result.put("cookies", cookies);
-            return result;
-        }
+        Map<String, String> result = new HashMap<>();
+        result.put("csrfToken", csrfToken);
+        result.put("cookies", cookies);
+        return result;
     }
 
     public void setCookiesAndCsrfToken(Map<String, String> cookiesAndCsrfToken) {
